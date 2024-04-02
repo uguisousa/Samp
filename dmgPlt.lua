ce6747071a86122d74da5cddc79cf312597bbbbd
Guilherme Sousa <123672664+uguisousa@users.noreply.github.com>
2024-04-02 15:12:43 -0300

    Update dmgPlt.lua

--- dmgPlt.lua
+++ dmgPlt.lua
@@ -3,6 +3,6 @@ function Damgeinfo(attacker, weapon, bodypart, loss)
     local weaponName = getWeaponNameFromID(weapon)
     local bodypartName = getBodyPartName(bodypart)
     
-    outputChatBox(" " .. attackerName .. "  " .. weaponName .. "  " .. bodypartName .. ".  " .. loss .. " .", source, 255, 0, 0)
+    outputChatBox(" " .. attackerName .. "  " .. weaponName .. "  " .. bodypartName .. ".  " .. loss .. " ", source, 255, 0, 0)
 end
 addEventHandler("Damgeinfo", root, Damgeinfo)
