e580265fc4c91d362f8b80baa0ca06a4c08a4c14
uguisousa <guilhermehssousa872@gmail.com>
2024-04-14 11:52:24 -0300

    Samp modification

---
+++ DamageInfo/Samp/dmgPlt.lua
@@ -0,0 +1,8 @@
+function Damgeinfo(attacker, weapon, bodypart, loss)
+    local attackerName = getPlayerName(attacker)
+    local weaponName = getWeaponNameFromID(weapon)
+    local bodypartName = getBodyPartName(bodypart)
+    
+    outputChatBox(" " .. attackerName .. "  " .. weaponName .. "  " .. bodypartName .. ".  " .. loss .. " ", source, 255, 0, 0)
+end
+addEventHandler("Damgeinfo", root, Damgeinfo)
